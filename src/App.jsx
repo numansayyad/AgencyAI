53326ce570f29b0af0a21b7af189af226048cc87
numan <numan@gmail.com>
2026-05-31 19:19:44 +0530

    i have create a work section in website

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import ServiceCard from './components/ServiceCard'
+import OurWork from './components/OurWork'
 
 const App = () => {
     // Initialize theme from localStorage, default to 'light'
@@ -18,6 +19,7 @@ const App = () => {
 
     <TrustedBy/>
     <Services/>
+    <OurWork/>
 
     </div>
   )
